62915aa8764052e72297fc3885fef59713053c35
CrazyBloo <CrazyBloo@users.noreply.github.com>
2021-05-09 22:19:59 -0400

    preview entity

---
+++ code/PreviewEntity.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Sandbox.Tools
+{
+	public class PreviewEntity : ModelEntity
+	{
+		public bool RelativeToNormal { get; set; } = true;
+		public bool OffsetBounds { get; set; } = false;
+		public Rotation RotationOffset { get; set; } = Rotation.Identity;
+		public Vector3 PositionOffset { get; set; } = Vector3.Zero;
+
+		internal bool UpdateFromTrace( TraceResult tr )
+		{
+			if ( !IsTraceValid( tr ) )
+			{
+				return false;
+			}
+
+			if ( RelativeToNormal )
+			{
+				WorldRot = Rotation.LookAt( tr.Normal, tr.Direction ) * RotationOffset;
+				WorldPos = tr.EndPos + WorldRot * PositionOffset;
+			}
+			else
+			{
+				WorldRot = Rotation.Identity * RotationOffset;
+				WorldPos = tr.EndPos + PositionOffset;
+			}
+
+			if ( OffsetBounds )
+			{
+				WorldPos += tr.Normal * CollisionBounds.Size * 0.5f;
+			}
+
+			return true;
+		}
+
+		protected virtual bool IsTraceValid( TraceResult tr ) => tr.Hit;
+	}
+}
