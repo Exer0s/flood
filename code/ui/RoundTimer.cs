4131193a4b4ee91504bc2b669b886a0092fd75c3
Exeros <Mikaeldufresne482@gmail.com>
2021-05-14 13:27:35 -0700

    revert round timer

--- code/ui/RoundTimer.cs
+++ code/ui/RoundTimer.cs
@@ -1,36 +1,33 @@
 using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
-using System;
 
-    public class RoundTimer : Panel
-    {
-        public Panel Container;
-        public Label RoundName;
-        public Label TimeLeft;
-        public Panel Icon;
+public class RoundTimer : Panel
+{
 
-        public RoundTimer()
-        {
-            StyleSheet.Load( "/ui/RoundTimer.scss" );
+	public Label Timer;
+	public Label roundLabel;
 
-            Container = Add.Panel( "roundContainer" );
-            RoundName = Container.Add.Label( "Round", "roundName" );
-            TimeLeft = Container.Add.Label( "00:00", "timeLeft" );
-        }
+	public RoundTimer()
+	{
+		Timer = Add.Label( "N/A", "timeLeft" );
+		roundLabel = Add.Label( "Waiting for Players...", "roundName" );
+		Log.Info( "Round Timer Created" );
+	}
 
-        public override void Tick()
-        {
-            var player = Sandbox.Player.Local;
-            if ( player == null ) return;
+	public override void Tick()
+	{
+		var player = Player.Local;
+		if ( player == null ) return;
 
-            var game = FloodGame.Instance;
-            if ( game == null ) return;
+		var game = FloodGame.Instance;
+		if ( game == null ) return;
 
-            var round = game.Round;
-            if ( round == null ) return;
+		var round = game.Round;
+		if ( round == null ) return;
 
-            RoundName.Text = round.RoundName;
-            TimeLeft.Text = round.TimeLeftFormatted;
-            }
-    }
+		Timer.Text = round.TimeLeftFormatted;
+		roundLabel.Text = round.RoundName;
+
+	}
+}
