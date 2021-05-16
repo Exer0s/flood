9037093023111d7c87ef2da16b677fa2f49df08c
CrazyBloo <CrazyBloo@users.noreply.github.com>
2021-05-16 14:19:53 -0400

    Post-Game Round

---
+++ code/rounds/PostGameRound.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Sandbox;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+	public class PostGameRound : BaseRound
+	{
+		public override string RoundName => "Post-Game";
+		[ServerVar( "flood_post_game_duration", Help = "The duration of the post-game round" )]
+		public override int RoundDuration => 15;
+		public override bool CanPlayerSuicide => true;
+		
+		private bool _isGameOver;
+
+		public override void OnPlayerKilled( FloodPlayer player )
+		{
+			player.Respawn();
+		}
+
+		public override void OnPlayerLeave( FloodPlayer player )
+		{
+			base.OnPlayerLeave( player );
+
+			Players.Remove( player );
+			Spectators.Remove(player);
+		}
+
+		public override void OnPlayerSpawn( FloodPlayer player )
+		{
+			player.ClearAmmo();
+			player.Inventory.DeleteContents();
+			if (!Players.Contains(player))
+			{
+				AddPlayer(player);
+			}
+			
+				
+				
+			base.OnPlayerSpawn( player );
+		}
+		
+		protected override void OnStart()
+		{
+
+			if ( Host.IsServer )
+			{
+				Sandbox.Player.All.ForEach( ( player ) => SupplyLoadouts( player as FloodPlayer ) );
+			}
+			
+		}
+
+		protected override void OnFinish()
+		{
+			if ( Host.IsServer )
+			{
+				Spectators.Clear();
+			}
+		}
+		
+
+	public override void OnSecond()
+	{
+		//base.OnSecond();
+	}
+
+	protected override void OnTimeUp()
+		{
+			if ( _isGameOver ) return;
+			
+			FloodGame.Instance.ChangeRound(new BuildRound());
+
+			base.OnTimeUp();
+		}
+
+		private void SupplyLoadouts( FloodPlayer player )
+		{
+			// Give everyone who is alive their starting loadouts.
+			//if ( player.LifeState == LifeState.Alive )
+			//{
+			player.ClearAmmo();
+			player.Inventory.DeleteContents();
+			if (!Players.Contains(player))
+			{
+				AddPlayer(player);
+			}
+			
+				//}
+		}
+	}
