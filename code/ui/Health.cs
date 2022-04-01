d46e30eb2b370d647ec9c6403b19e6c28342a3bd
CrazyBloo <CrazyBloo@users.noreply.github.com>
2022-04-01 14:57:51 -0400

    profile picture + money on healthbar

--- code/ui/Health.cs
+++ code/ui/Health.cs
@@ -9,6 +9,8 @@ public class Health : Panel
 	Panel whiteBar;
 	Panel backBar;
 	Label healthLabel;
+	Label Money;
+	Image Avatar;
 
 	public Health()
 	{
@@ -23,9 +25,13 @@ public class Health : Panel
 	{
 		var p = Local.Pawn as FloodPlayer;
 		if ( p == null ) return;
-		var health = Length.Percent( (p.Health / p.maxHealth) * 100 );
-		healthLabel.Text = $"{p.Health} / {p.maxHealth}";
+		if (Avatar == null ) Avatar = Add.Image( $"avatar:{Local.Client.PlayerId}", "avatar" );
+		if (Money == null ) Money = Add.Label( $"${p.Money}", "money" );
+		var health = Length.Percent( p.Health / p.maxHealth * 100 );
+		healthLabel.Text = $"{p.Health.CeilToInt()} / {p.maxHealth.CeilToInt()}";
+		if ( Money != null ) Money.Text = $"${p.Money}";
 		healthBar.Style.Width = health;
+		//backBar.Style.Width = health;
 		whiteBar.Style.Width = health;
 		//Log.Info( p.Health / 100 );
 		base.Tick();
